c86efbc01affda55a68cd850f8fba1acf0fd0638
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2024-10-09 00:12:34 +0100

    reorganised files

---
+++ public/js/lessons.js
@@ -0,0 +1,35 @@
+let lessons = [
+    { id:1, subject: 'Math', location: 'London', price : 20, availability: 5, image: 'mathIcon.png'},
+    { id:2, subject: 'Math', location: 'Birmingham', price : 25, availability: 5, image: 'mathIcon.png'},
+    { id:3, subject: 'Math', location: 'Brighton', price : 15, availability: 5, image: 'mathIcon.png'},
+    { id:4, subject: 'Math', location: 'Liverpool', price : 25, availability: 5, image: 'mathIcon.png'},
+    { id:5, subject: 'Math', location: 'Newcastle', price : 20, availability: 5, image: 'mathIcon.png'},
+
+    { id:6, subject: 'English', location: 'London', price : 20, availability: 5, image: 'englishIcon.png'},
+    { id:7, subject: 'English', location: 'Birmingham', price : 25, availability: 5, image: 'englishIcon.png'},
+    { id:8, subject: 'English', location: 'Brighton', price : 15, availability: 5, image: 'englishIcon.png'},
+    { id:9, subject: 'English', location: 'Liverpool', price : 25, availability: 5, image: 'englishIcon.png'},
+    { id:10, subject: 'English', location: 'Newcastle', price : 20, availability: 5, image: 'englishIcon.png'},
+
+    
+    { id:11, subject: 'Biology', location: 'London', price : 20, availability: 5, image: 'biologyIcon.png'},
+    { id:12, subject: 'Biology', location: 'Birmingham', price : 25, availability: 5, image: 'biologyIcon.png'},
+    { id:13, subject: 'Biology', location: 'Brighton', price : 15, availability: 5, image: 'biologyIcon.png'},
+    { id:14, subject: 'Biology', location: 'Liverpool', price : 25, availability: 5, image: 'biologyIcon.png'},
+    { id:15, subject: 'Biology', location: 'Newcastle', price : 20, availability: 5, image: 'biologyIcon.png'},
+
+    
+    { id:16, subject: 'Chemistry', location: 'London', price : 20, availability: 5, image: 'chemistryIcon.png'},
+    { id:17, subject: 'Chemistry', location: 'Birmingham', price : 25, availability: 5, image: 'chemistryIcon.png'},
+    { id:18, subject: 'Chemistry', location: 'Brighton', price : 15, availability: 5, image: 'chemistryIcon.png'},
+    { id:19, subject: 'Chemistry', location: 'Liverpool', price : 25, availability: 5, image: 'chemistryIcon.png'},
+    { id:20, subject: 'Chemistry', location: 'Newcastle', price : 20, availability: 5, image: 'chemistryIcon.png'},
+
+    
+    { id:21, subject: 'Physics', location: 'London', price : 20, availability: 5, image: 'physicsIcon.png'},
+    { id:22, subject: 'Physics', location: 'Birmingham', price : 25, availability: 5, image: 'physicsIcon.png'},
+    { id:23, subject: 'Physics', location: 'Brighton', price : 15, availability: 5, image: 'physicsIcon.png'},
+    { id:24, subject: 'Physics', location: 'Liverpool', price : 25, availability: 5, image: 'physicsIcon.png'},
+    { id:25, subject: 'Physics', location: 'Newcastle', price : 20, availability: 5, image: 'physicsIcon.png'},
+    
+];

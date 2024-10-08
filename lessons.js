b3a5580bb4de86a446159040874ccf48cac4889a
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2024-10-08 18:43:54 +0100

    created a file to store multiple lesson data

---
+++ lessons.js
@@ -0,0 +1,7 @@
+let lessons = [
+    { id:'1', subject: 'Math', location: 'London', price : '20', availability: '5', image: 'mathIcon.png'},
+    { id:'2', subject: 'Math', location: 'Birmingham', price : '25', availability: '5', image: 'mathIcon.png'},
+    { id:'3', subject: 'Math', location: 'Brighton', price : '15', availability: '5', image: 'mathIcon.png'},
+    { id:'4', subject: 'Math', location: 'Liverpool', price : '25', availability: '5', image: 'mathIcon.png'},
+    { id:'5', subject: 'Math', location: 'Newcastle', price : '20', availability: '5', image: 'mathIcon.png'},
+];
